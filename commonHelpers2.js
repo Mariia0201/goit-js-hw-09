9ee736552ac107732e51e7f596a6bea5222141d1
Mariia0201 <134952176+Mariia0201@users.noreply.github.com>
2024-01-29 14:06:02 +0000

    Deploying to gh-pages from @ Mariia0201/goit-js-hw-09@698fdeb928427944a0eb6e67a91d0556c7ff86d0 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,14 @@
-import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */const m=document.getElementById("form-container");m.innerHTML=createFormMarkup();const n=document.querySelector(".feedback-form"),r="feedback-form-state";n.addEventListener("input",l);n.addEventListener("submit",u);let t=s();function l(a){const{email:e,message:o}=n.elements;t={email:e.value.trim(),message:o.value.trim()},localStorage.setItem(r,JSON.stringify(t))}function s(){return JSON.parse(localStorage.getItem(r))||{}}function i(){const{email:a,message:e}=n.elements;t&&(a.value=t.email||"",e.value=t.message||"")}function u(a){a.preventDefault();const{email:e,message:o}=n.elements;if(e.value.trim()===""||o.value.trim()==="")return alert("Please fill in all the fields!");console.log({email:e.value,message:o.value}),localStorage.removeItem(r),a.currentTarget.reset(),t={}}i();
+import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */function n(){return`
+    <form class="feedback-form" autocomplete="off">
+      <label>
+        Email
+        <input type="email" name="email" autofocus />
+      </label>
+      <label>
+        Message
+        <textarea name="message" rows="8"></textarea>
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  `}const r=document.getElementById("form-container");r.innerHTML=n();const o=document.querySelector(".feedback-form"),l="feedback-form-state";o.addEventListener("input",s);o.addEventListener("submit",c);let t=i();function s(a){const{email:e,message:m}=o.elements;t={email:e.value.trim(),message:m.value.trim()},localStorage.setItem(l,JSON.stringify(t))}function i(){return JSON.parse(localStorage.getItem(l))||{}}function u(){const{email:a,message:e}=o.elements;t&&(a.value=t.email||"",e.value=t.message||"")}function c(a){a.preventDefault();const{email:e,message:m}=o.elements;if(e.value.trim()===""||m.value.trim()==="")return alert("Please fill in all the fields!");console.log({email:e.value,message:m.value}),localStorage.removeItem(l),a.currentTarget.reset(),t={}}u();
 //# sourceMappingURL=commonHelpers2.js.map
